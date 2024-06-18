a4584805dd6af9da8152b13c2e4bca0ee6e3b77a
lawtronicus <46512764+lawtronicus@users.noreply.github.com>
2024-06-18 09:49:10 +0900

    added end to end test for 'filter events by city' feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -36,4 +36,65 @@ describe("show/hide event details", () => {
   });
 });
 
+describe("Filter Events by City", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 0,
+      timeout: 0,
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".city");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("I see upcoming events for all cities by default up to the default limit (32)", async () => {
+    const events = await page.$$eval("#event-list .event", (events) => {
+      return events.map((event) => ({
+        summary: event.querySelector("h2").innerText,
+        location: event.querySelector("p").innerText,
+      }));
+    });
+
+    expect(events.length).toBeGreaterThan(0);
+    expect(events.length).toBeLessThanOrEqual(32);
+
+    const cities = events.map((event) => event.location);
+    const uniqueCities = [...new Set(cities)];
+    expect(uniqueCities.length).toBeGreaterThan(1);
+  });
+
+  test("User will see list cities that match what they typed so far", async () => {
+    await page.type(".city", "be");
+    await page.waitForSelector(".suggestions");
+    const matchingCities = await page.$$eval(".suggestions li", (items) =>
+      items.map((item) => item.textContent),
+    );
+    expect(matchingCities).toContain("Berlin, Germany");
+  });
+
+  test("When user clicks on a possible city the user will see all events from that city", async () => {
+    // clear "be" from previous test
+    await page.evaluate(() => {
+      document.querySelector(".city").value = "";
+    });
+    await page.type(".city", "be");
+    await page.waitForSelector(".suggestions");
+    await page.click(".suggestions li", { text: "Berlin, Germany" });
+    await page.waitForSelector("#event-list");
+    const events = await page.$$eval("#event-list .event", (events) =>
+      events.map((event) => event.querySelector("p").textContent),
+    );
+    events.forEach((location) => {
+      expect(location).toBe("Berlin, Germany");
+    });
+  });
+});
+
 jest.setTimeout(30000);
