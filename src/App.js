15c77d98de69ded6459cf1ae5e9f5693859e69dd
lawtronicus <46512764+lawtronicus@users.noreply.github.com>
2024-06-25 10:24:12 +0900

    added error handling to getEvent function in app.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ const App = () => {
 
   const fetchData = async () => {
     const allEvents = await getEvents();
+    if (!allEvents) {
+      // Handle the case when getEvents returns undefined or null
+      console.error("Failed to fetch events.");
+      return;
+    }
+
     const filteredEvents =
       currentCity === "See all cities"
         ? allEvents
